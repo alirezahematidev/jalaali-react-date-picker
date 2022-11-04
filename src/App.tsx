2855c276d09bcbd2d0f3f11bde37f784d90cf6c8
alirezahematidev <alirezahematideveloper71@gmail.com>
2022-11-04 18:04:29 +0330

    reset app

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,5 @@
-import Day from "./components/day";
-import { useSetColors } from "./core/hooks/useSetColors";
-import { ColorSchema } from "./core/types/global.types";
-import "./styles/index.scss";
-
 function App() {
-  const colors: ColorSchema = {};
-
-  useSetColors(colors);
-
-  return (
-    <div style={{ padding: 50 }}>
-      <Day id="1" day={1} monthId="1" />
-    </div>
-  );
+  return <div>app</div>;
 }
 
 export default App;
