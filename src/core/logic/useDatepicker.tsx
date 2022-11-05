f4e9231d12dc97fed427ede2e24dd033d034645f
alirezahematidev <alirezahematideveloper71@gmail.com>
2022-11-05 23:50:59 +0330

    dayoff

--- src/core/logic/useDatepicker.tsx
+++ src/core/logic/useDatepicker.tsx
@@ -1,8 +1,9 @@
-import { generateDays } from "../../utils/generateDays";
+import { generateDays } from "../../utils";
 import { useDatePickerContext } from "../context";
 
 export const useDatepicker = () => {
   const { state, ...rest } = useDatePickerContext();
+
   const { days } = generateDays(state.month, state.year);
 
   return {
