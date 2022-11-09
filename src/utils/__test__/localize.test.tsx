4962209538fbc9ee2950ccea0af8f8377f5245eb
mehdi <75084418+samimideveloper@users.noreply.github.com>
2022-11-09 13:56:58 +0330

    change language test resolve

--- src/utils/__test__/localize.test.tsx
+++ src/utils/__test__/localize.test.tsx
@@ -1,7 +1,45 @@
 import { useDatepicker } from "../../core";
 import { gregorianMonths, jalaaliMonths } from "../../core/constants/datasets";
-import { renderHook } from "@testing-library/react";
+import {
+  renderHook,
+  render,
+  getByTestId,
+  fireEvent,
+} from "@testing-library/react";
 import { Provider } from "../../core/context/index";
+import { shallow } from "enzyme";
+import React, { useState } from "react";
+
+import Adapter from "enzyme-adapter-react-16";
+import { configure } from "enzyme";
+
+configure({ adapter: new Adapter() });
+
+const ChangeLanguage = () => {
+  const [lang, setLang] = useState("fa");
+
+  const handleChangeLanguage = () => setLang("en");
+
+  return (
+    <div className="App">
+      <p data-testid="language">{lang}</p>
+
+      <button data-testid="onButton" onClick={handleChangeLanguage}>
+        click
+      </button>
+    </div>
+  );
+};
+
+describe("change language ", () => {
+  test("click test", () => {
+    const { container } = render(<ChangeLanguage />);
+    const language = getByTestId(container, "language");
+    const onButton = getByTestId(container, "onButton");
+    fireEvent.click(onButton);
+    expect(language.textContent).toBe("en");
+  });
+});
 
 describe("locaize test", () => {
   const { result } = renderHook(() => useDatepicker(), {
