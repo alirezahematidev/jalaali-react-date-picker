11bf8af82b46127d501bc9246d1d448eb92e81ba
Mohammad Gharouni <mohammadgharouni@icloud.com>
2022-11-14 14:58:53 +0330

    moved range header

--- src/components/rangeDays/index.tsx
+++ src/components/rangeDays/index.tsx
@@ -4,7 +4,7 @@ import { useRangeDays, useRangepicker } from "../../core";
 import { useRangePanelContext } from "../rangePanel/panelRangeMode";
 import { RangeDayPanel } from "./rangeDayPanel";
 import { Date } from "../../core/types/global.types";
-import { RangeHeader } from "../rangeHeader";
+import { RangeHeader } from "../rangeHeader/header";
 import { useRangeTemplate } from "../rangePanel/templateContext";
 
 function validateRangeDates(startDate: Date, endDate: Date) {
