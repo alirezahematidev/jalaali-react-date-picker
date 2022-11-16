58ce99660b4244f94550f0a57d659b9981024dd2
Mohammad Gharouni <mohammadgharouni@icloud.com>
2022-11-16 14:45:50 +0330

    moved test

---
+++ src/core/context/date/__tests__/useDateReducer.test.tsx
@@ -0,0 +1,141 @@
+import { act, renderHook } from "@testing-library/react";
+import { DateProvider, useDateReducer } from "../../..";
+
+describe("onDaychange", () => {
+  test("test useReducer", async () => {
+    const { result } = renderHook(() => useDateReducer({ language: "fa" }), {
+      wrapper({ children }) {
+        return (
+          <DateProvider props={{ locale: { language: "fa" } }}>
+            {children}
+          </DateProvider>
+        );
+      },
+    });
+    act(() => result.current.onDaychange({ day: 5, month: 7, year: 1401 }));
+
+    expect(result.current.cacheDate).toStrictEqual({
+      day: 5,
+      month: 7,
+      year: 1401,
+    });
+  });
+});
+
+describe("onDecreaseMonth", () => {
+  test("test useReducer", async () => {
+    const { result } = renderHook(() => useDateReducer({ language: "fa" }), {
+      wrapper({ children }) {
+        return (
+          <DateProvider props={{ locale: { language: "fa" } }}>
+            {children}
+          </DateProvider>
+        );
+      },
+    });
+    act(() => result.current.onDecreaseMonth({ day: 0, month: 8, year: 1401 }));
+    expect(result.current.state).toStrictEqual({
+      day: 0,
+      month: 7,
+      year: 1401,
+    });
+  });
+});
+
+describe("onIncreaseMonth", () => {
+  test("test useReducer", async () => {
+    const { result } = renderHook(() => useDateReducer({ language: "fa" }), {
+      wrapper({ children }) {
+        return (
+          <DateProvider props={{ locale: { language: "fa" } }}>
+            {children}
+          </DateProvider>
+        );
+      },
+    });
+    act(() => result.current.onIncreaseMonth({ day: 0, month: 4, year: 1401 })),
+      expect(result.current.state).toStrictEqual({
+        day: 0,
+        month: 5,
+        year: 1401,
+      });
+  });
+});
+describe("onYearchange", () => {
+  test("test useReducer", async () => {
+    const { result } = renderHook(() => useDateReducer({ language: "fa" }), {
+      wrapper({ children }) {
+        return (
+          <DateProvider props={{ locale: { language: "fa" } }}>
+            {children}
+          </DateProvider>
+        );
+      },
+    });
+    act(() => result.current.onYearchange({ day: 0, month: 0, year: 1401 }));
+
+    expect(result.current.state).toStrictEqual({
+      day: 0,
+      month: 8,
+      year: 1401,
+    });
+  });
+});
+describe("onMonthchange", () => {
+  test("test useReducer", async () => {
+    const { result } = renderHook(() => useDateReducer({ language: "fa" }), {
+      wrapper({ children }) {
+        return (
+          <DateProvider props={{ locale: { language: "fa" } }}>
+            {children}
+          </DateProvider>
+        );
+      },
+    });
+    act(() => result.current.onMonthchange({ day: 0, month: 8, year: 1401 }));
+
+    expect(result.current.state).toStrictEqual({
+      day: 0,
+      month: 8,
+      year: 1401,
+    });
+  });
+});
+describe("onIncreaseYear", () => {
+  test("test useReducer", async () => {
+    const { result } = renderHook(() => useDateReducer({ language: "fa" }), {
+      wrapper({ children }) {
+        return (
+          <DateProvider props={{ locale: { language: "fa" } }}>
+            {children}
+          </DateProvider>
+        );
+      },
+    });
+    act(() => result.current.onIncreaseYear({ day: 1, month: 7, year: 1401 }));
+    expect(result.current.state).toStrictEqual({
+      day: 0,
+      month: 8,
+      year: 1402,
+    });
+  });
+});
+describe("onDecreaseYear", () => {
+  test("test useReducer", async () => {
+    const { result } = renderHook(() => useDateReducer({ language: "fa" }), {
+      wrapper({ children }) {
+        return (
+          <DateProvider props={{ locale: { language: "fa" } }}>
+            {children}
+          </DateProvider>
+        );
+      },
+    });
+    act(() => result.current.onDecreaseYear({ day: 1, month: 9, year: 1401 }));
+    expect(result.current.state).toStrictEqual({
+      day: 0,
+      month: 8,
+      year: 1400,
+    });
+  });
+});
