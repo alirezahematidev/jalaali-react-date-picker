ced8d78e189d0c18265985e2b475605550a65342
Mohammad Gharouni <mohammadgharouni@icloud.com>
2022-11-16 14:44:00 +0330

    usedays test

---
+++ src/core/hooks/date/__tests__/useDays.test.tsx
@@ -0,0 +1,37 @@
+import { renderHook } from "@testing-library/react";
+import moment from "moment-jalaali";
+import { ReactNode } from "react";
+import { DateProvider, useDays } from "../../..";
+
+function wrapper({ children }: { children: ReactNode }) {
+  return (
+    <DateProvider
+      props={{
+        locale: { language: "fa" },
+        disabledDates: (current) => {
+          return current.isBefore(moment());
+        },
+      }}
+    >
+      {children}
+    </DateProvider>
+  );
+}
+
+describe("useDays", () => {
+  test("test useDays", async () => {
+    const {
+      result: {
+        current: { days },
+      },
+    } = renderHook(() => useDays(), { wrapper });
+
+    expect(days).toHaveLength(42);
+    expect(
+      days.some(({ isNotCurrentMonth }) => isNotCurrentMonth),
+    ).toBeTruthy();
+    expect(days.filter((item) => item.isDisabled).slice(-1)[0].day).toBe(
+      moment().jDate(),
+    );
+  });
+});
