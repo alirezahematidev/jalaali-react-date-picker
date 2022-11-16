b3b1a43c8cbd0e24bdd3ed781755f31e85c80061
Mohammad Gharouni <mohammadgharouni@icloud.com>
2022-11-16 14:44:12 +0330

    useyeras test

---
+++ src/core/hooks/date/__tests__/useYears.test.tsx
@@ -0,0 +1,46 @@
+import { renderHook } from "@testing-library/react";
+import moment from "moment-jalaali";
+import { ReactNode } from "react";
+import { DateProvider, useYears } from "../../..";
+
+function wrapper({ children }: { children: ReactNode }) {
+  return (
+    <DateProvider
+      props={{
+        locale: { language: "fa" },
+        disabledDates: (current) => {
+          return current.isBefore(moment());
+        },
+      }}
+    >
+      {children}
+    </DateProvider>
+  );
+}
+
+describe("useYears", () => {
+  test("test useYears", async () => {
+    const {
+      result: {
+        current: { years },
+      },
+    } = renderHook(() => useYears(10), { wrapper });
+
+    const {
+      result: {
+        current: { years: lastDecade },
+      },
+    } = renderHook(() => useYears(-10), { wrapper });
+    const {
+      result: {
+        current: { years: nextDecade },
+      },
+    } = renderHook(() => useYears(10), { wrapper });
+
+    expect(years).toHaveLength(12);
+    expect(years[0].isNotCurrentDecade).toBeTruthy();
+    expect(years[years.length - 1].isNotCurrentDecade).toBeTruthy();
+    expect(lastDecade.every((year) => year.isDisabled)).toBeTruthy();
+    expect(nextDecade.every((year) => year.isDisabled)).toBeFalsy();
+  });
+});
