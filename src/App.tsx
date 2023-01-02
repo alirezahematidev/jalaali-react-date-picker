13056a77adfa57d5033978d133f63e1fd52d5724
a.hemati <alirezahematideveloper71@gmail.com>
2023-01-02 17:29:30 +0330

    dev , build scripts

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,5 @@
-import { InputRangePicker } from "./components/rangeInput";
-
 function App() {
-  return (
-    <div
-      style={{
-        // direction: "rtl",
-        // // padding: 50,
-        // marginTop: 450,
-        // marginLeft: 100,
-        // gap: 20,
-        display: "flex",
-        height: "100%",
-        justifyContent: "flex-start",
-        alignItems: "flex-start",
-        // alignItems: "flex-start",
-      }}
-    >
-      <InputRangePicker placement="right" />
-      {/* <InputRangePicker placement="right" /> */}
-    </div>
-  );
+  return <div>app</div>;
 }
 
 export default App;
--- src/index.tsx
+++ src/index.tsx
@@ -1,18 +1,13 @@
-import { DatePicker } from "@components/date";
-import { InputDatePicker } from "@components/dateInput";
-import { RangePicker } from "@components/range";
-import { InputRangePicker } from "@components/rangeInput";
-import {
-  DatePickerProps,
-  InputDatePickerProps,
-  InputRangePickerProps,
-  RangePickerProps,
-} from "@core/interfaces";
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "src/styles/index.css";
+import App from "./App";
 
-export { DatePicker, RangePicker, InputDatePicker, InputRangePicker };
-export type {
-  DatePickerProps,
-  RangePickerProps,
-  InputDatePickerProps,
-  InputRangePickerProps,
-};
+const root = ReactDOM.createRoot(
+  document.getElementById("root") as HTMLElement,
+);
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+);
