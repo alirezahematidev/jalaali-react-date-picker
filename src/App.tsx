32d2d2da06b0a1bf505b6b199d928a4ceecb36cb
Mohammad Gharouni <mohammadgharouni@icloud.com>
2023-01-03 10:47:05 +0330

    remove redundant

--- src/App.tsx
+++ src/App.tsx
@@ -9,33 +9,3 @@ function App() {
 }
 
 export default App;
-
-const a = [
-  "Moment",
-  "MonthValue",
-  "MonthRangeValue",
-  "PanelDate",
-  "Mode",
-  "Colors",
-  "RangeValue",
-  "DefaultRangeValue",
-  "OnChange",
-  "OnDayChange",
-  "OnMonthChange",
-  "OnYearChange",
-  "OnModeChange",
-  "DisabledDates",
-  "DayRender",
-  "HighlightDays",
-  "Weekend",
-  "Locale",
-  "Format",
-  "HeaderRender",
-  "PanelRender",
-  "DayLabelRender",
-];
-console.log(
-  a
-    .map((item) => item.split("")[0].toLowerCase() + item.slice(1, item.length))
-    .join("\n"),
-);
