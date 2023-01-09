059c0825ec77ac7fb2e1511c5d1ce9e2e3fe93a8
Mohammad Gharouni <mohammadgharouni@icloud.com>
2023-01-09 11:19:25 +0330

    fix test

--- src/index.tsx
+++ src/index.tsx
@@ -1,13 +1,19 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import App from "./App";
-import "./styles/index.css";
+import { DatePicker } from "./components/date";
+import { InputDatePicker } from "./components/dateInput";
+import { RangePicker } from "./components/range";
+import { InputRangePicker } from "./components/rangeInput";
 
-const root = ReactDOM.createRoot(
-  document.getElementById("root") as HTMLElement,
-);
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-);
+import {
+  DatePickerProps,
+  InputDatePickerProps,
+  InputRangePickerProps,
+  RangePickerProps,
+} from "./core/interfaces";
+
+export { DatePicker, RangePicker, InputDatePicker, InputRangePicker };
+export type {
+  InputDatePickerProps,
+  InputRangePickerProps,
+  RangePickerProps,
+  DatePickerProps,
+};
